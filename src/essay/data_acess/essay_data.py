4a145be1470df1f9b7d3914a7faa5540aa3a0a05
nishant129 <nishantborkar139@gmail.com>
2024-05-02 15:45:49 +0530

    Ok

--- src/essay/data_acess/essay_data.py
+++ src/essay/data_acess/essay_data.py
@@ -41,7 +41,7 @@ class ExtractData:
             df = pd.DataFrame(list(collection.find()))
             
             if "_id" in df.columns.to_list():
-                df = df.drop(columns=["_id"], axis=1)
+                df = df.drop(columns=["_id","essay_id"], axis=1)
             df.replace({"na": np.nan}, inplace=True)
             
             
